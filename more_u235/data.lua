70b24a95e6518fb8a5bae9df3c975068ddec0b54
toxicantidote <23189273+toxicantidote@users.noreply.github.com>
2022-03-20 09:21:50 +1030

    Version 0.0.6
    No more settings or fancy customisable probability.

---
+++ more_u235/data.lua
@@ -0,0 +1,6 @@
+--[[
+	Part of the 'More uranium-235' mod.
+	
+	See LICENSE.txt in the root mod folder for licensing details.
+--]]
+require("prototypes.recipe")
---
+++ more_u235/prototypes/recipe.lua
@@ -0,0 +1,48 @@
+--[[
+	Part of the 'More uranium-235' mod.
+	
+	See LICENSE.txt in the root mod folder for licensing details.
+--]]
+data:extend(
+	{
+		
+		  {
+			type = "recipe",
+			name = "uranium-processing",
+			energy_required = 18,
+			enabled = false,
+			category = "centrifuging",
+			ingredients =
+                {
+                    {"uranium-ore", 10}
+                },
+			icon = "__base__/graphics/icons/uranium-processing.png",
+			icon_size = 64, icon_mipmaps = 4,
+			subgroup = "raw-material",
+			order = "k[uranium-processing]",
+			results =
+                {
+                    {"uranium-235", 2},
+                    {"uranium-238", 2},
+                }
+		  },
+		  {
+			type = "recipe",
+			name = "kovarex-enrichment-process",
+			energy_required = 90,
+			enabled = false,
+			category = "centrifuging",
+			ingredients = {{"uranium-235", 40}, {"uranium-238", 5}},
+			icon = "__base__/graphics/icons/kovarex-enrichment-process.png",
+			icon_size = 64, icon_mipmaps = 4,
+			subgroup = "intermediate-product",
+			order = "r[uranium-processing]-c[kovarex-enrichment-process]",
+			main_product = "",
+			results =
+                {
+                    {"uranium-235", 45},
+                },
+			allow_decomposition = false
+		  },
+	}
+)
